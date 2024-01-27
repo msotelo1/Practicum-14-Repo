c3bd40f864f5650c929cc198a6780766f6bc925a
Chuck476 <cdierbach@towson.edu>
2024-01-26 19:48:41 -0500

    Create main_solution.java

---
+++ main_solution.java
@@ -0,0 +1,28 @@
+// Practicum 14 (The Enhanced for Loop in Java) Solution
+
+// This program will add up all the integers in a given array.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // array declaration
+        // declares an array of size 6 initialized
+        // with the provided values
+        int[] nums = {5, 24, 36, 54, 65, 72};
+     
+        // provide an enhanced for loop that adds up all
+        // values in array nums with the result in variable sum
+        int sum = 0;
+
+        // enhanced for loop (complete)
+        for(int i: nums)
+          sum = sum + i;
+
+        // display results
+        System.out.println("Sum = " + sum);
+
+    }
+}
